c788d65b466eff25375f28193445bcdc946a1ab7
Muhammad Adil <141252873+adil273@users.noreply.github.com>
2023-08-21 09:35:38 -0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,19 @@
+"use strict";
+//ARRAY DECLARATION WITH NUMBERS
+var a = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+{
+    console.log("Array with Number, the value is =", a[2]);
+}
+//ARRAY DECLARATION WITH STRING
+var b;
+b = ["Adil", "Aslam", "Ahmad", "Ali", "Abdullah"];
+{
+    console.log("Array with String, the value is =", b[2], " and ", b[3]);
+}
+//ARRAY DECLARATION WITH FOR LOOP
+var c = [1, 2, 3, 4, 5];
+{
+    for (var i = 0; i < 5; i++) {
+        console.log(c[i]);
+    }
+}
---
+++ array.ts
@@ -0,0 +1,20 @@
+//ARRAY DECLARATION WITH NUMBERS
+var a:number[]=[1,2,3,4,5,6,7,8,9,];
+{
+    console.log("Array with Number, the value is =", a[2]);
+}
+
+//ARRAY DECLARATION WITH STRING
+var b:string[];
+b=["Adil","Aslam","Ahmad","Ali","Abdullah"];
+{
+    console.log("Array with String, the value is =", b[2]," and ", b[3]);
+}
+
+//ARRAY DECLARATION WITH FOR LOOP
+var c:number[]=[1,2,3,4,5];
+{
+    for(var i:number=0; i<5; i++){
+        console.log(c[i]);
+    }
+}
